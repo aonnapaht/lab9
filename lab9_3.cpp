a62c080d7f2c372f8e37953767f401e40fa3f698
aonnapaht <napaht_d@cmu.ac.th>
2022-12-23 16:19:01 +0700

    lab9_3

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,63 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    double height;
+    int bounty;
+    string zoro="Zoro";
+    string sanji="Sanji";
+    string usopp= "Usopp";
+    string chopper="Chopper";
+    string franky="Franky";
+    string brook="Brook";
+    string jinbe="Jinbe";
+    cout<<"Enter your age: ";
+    
+    cin>>age;
+    if(age<=25)
+    {
+        cout<<"Enter your height: ";
+        cin>>height;
+        if(height<100)
+        {
+            cout<<"Your character = "<<chopper;
+        }
+        else if(height<180 and height>=100)
+        {
+            cout<<"Your character = "<<usopp;
+        }
+        else
+        {
+            cout<<"Enter your bounty: ";
+            cin>>bounty;
+            if(bounty<=1100000000)
+            {
+                cout<<"Your character = "<<sanji;
+            }
+            else
+            {
+                cout<<"Your character = "<<zoro;
+            }
+        }
+    }
+    else if(age<=60 and age >25)
+    {
+        cout<<"Enter your bounty: ";
+        cin>>bounty;
+        if(bounty<=500000000)
+        {
+           cout<<"Your character = "<<franky; 
+        }
+        else
+        {
+            cout<<"Your character = "<<jinbe;
+        }
+    }
+    else
+    {
+        cout<<"Your character = "<<brook;
+    }
+    return 0;
+}
